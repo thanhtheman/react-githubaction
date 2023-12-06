dc0086a57d8260d3aedf5644c1413e2fefc982b6
thanhtheman <thanhb.quach@gmail.com>
2023-12-06 11:26:24 -0500

    adding an h2 to create a pull request

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h2>I added this line to create a pull request to merge with the main branch</h2>
       </header>
       <MainContent />
     </>
