cbde2f947d09bcefc60300ca821cc2d83e099d17
thanhtheman <thanhb.quach@gmail.com>
2023-12-06 11:47:06 -0500

    adding h3 to push changes to actiivty_type_pullrequest branch

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         </div>
         <h1>Learn & Master GitHub Actions</h1>
         <h2>I added this line to create a pull request to merge with the main branch</h2>
+        <h3>I added this line to trigger the filter event workflow when we push this change to the activity_type_pullrequest branch</h3>
       </header>
       <MainContent />
     </>
